12dbc3d82b3851666caeed4ebb7650fa860dc2e4
ReblochonMasque <trustgold@gmail.com>
2018-02-26 15:44:33 +0800

    add getitem

--- src/vector.py
+++ src/vector.py
@@ -9,12 +9,16 @@ class Vector2D:
     def __init__(self, x: float =0, y: float =0, epsilon: float =EPSILON)-> None:
         self.x = x
         self.y = y
+        self.v = (self.x, self.y)
         self.epsilon = epsilon
 
     def __eq__(self, other):
         assert other is not None
         return math.isclose(self.x, other.x, abs_tol=self.epsilon) and \
                math.isclose(self.y, other.y, abs_tol=self.epsilon)
+
+    def __getitem__(self, ndx):
+        return self.v[ndx]
 
 
 if __name__ == '__main__':
