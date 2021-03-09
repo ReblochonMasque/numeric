cd0b1574dc1f1797503c89738e8ed2807c16ba25
ReblochonMasque <trustgold@gmail.com>
2021-03-09 12:36:54 +0800

    change animation

--- src/satelliteimagerotate.py
+++ src/satelliteimagerotate.py
@@ -10,20 +10,19 @@ class SimpleApp:
         self.canvas = tk.Canvas(master, width=500, height=500)
         self.canvas.pack()
 
-        self.update = self.draw().__next__
-        master.after(100, self.update)
+        self.tkimage = ImageTk.PhotoImage(self.image)
+        self.canvas_obj = self.canvas.create_image(250, 250, image=self.tkimage)
+        self.angle = 0
+        self.master.after(10, self.draw())
 
     def draw(self):
-        angle = 0
-        while True:
-            tkimage = ImageTk.PhotoImage(self.image.rotate(angle))
-            canvas_obj = self.canvas.create_image(
-                250, 250, image=tkimage)
-            self.master.after_idle(self.update)
-            yield
-            self.canvas.delete(canvas_obj)
-            angle += 1
-            angle %= 360
+        self.canvas.delete(self.canvas_obj)
+        self.tkimage = ImageTk.PhotoImage(self.image.rotate(self.angle))
+        self.canvas_obj = self.canvas.create_image(250, 250, image=self.tkimage)
+        self.master.after_idle(self.master.update)
+        self.angle += 1
+        self.angle %= 360
+        self.master.after(10, self.draw)
 
 
 def resize_image(image, width):
@@ -35,7 +34,7 @@ def resize_image(image, width):
 if __name__ == '__main__':
 
     root = tk.Tk()
-    path = '/Volumes/Extended/Docs/projects/threedee/threedee/resources/'
+    path = '../resources/'
     # img_name = path + 'satellite_2.png'
     img_name = path + 'USS_Enterprise.png'
 
