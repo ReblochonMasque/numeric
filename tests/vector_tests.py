7ee79828f55bc23e709736a0dc12109d96554a2d
ReblochonMasque <trustgold@gmail.com>
2021-03-10 10:49:38 +0800

    add test_floordiv_0 & test_ifloordiv_0

--- tests/vector_tests.py
+++ tests/vector_tests.py
@@ -106,6 +106,20 @@ class TestVector2D(unittest.TestCase):
         self.assertEqual(expected, actual)
         self.assertEqual(expected_id, actual_id)
 
+    def test_floordiv_0(self):
+        expected = Vector2D(1.//3., -4.//3.)
+        actual = Vector2D(-1, 4) // -3
+        self.assertEqual(expected, actual)
+
+    def test_ifloordiv_0(self):
+        expected = Vector2D(-1.//3., 4.//3.)
+        actual = Vector2D(-1, 4)
+        expected_id = id(actual)
+        actual //= 3
+        actual_id = id(actual)
+        self.assertEqual(expected, actual)
+        self.assertEqual(expected_id, actual_id)
+
     def test_dot_1(self):
         actual = Vector2D(1, 0).dot(Vector2D(0, 1))
         expected = 0
