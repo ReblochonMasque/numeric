77993ec04ddb853847e50bc8ffdefc9c680b1bec
ReblochonMasque <trustgold@gmail.com>
2021-03-12 11:32:09 +0800

    add test_instance_1()

--- tests/test_point.py
+++ tests/test_point.py
@@ -8,6 +8,9 @@ class TestAbstractVector2DWithPoint2D(unittest.TestCase):
     def test_instance_0(self):
         self.assertIsInstance(Point2D(), Point2D)
 
+    def test_instance_1(self):
+        self.assertIsInstance(Point2D(x=2.2, y=-3.7), Point2D)
+
 
 if __name__ == '__main__':
     unittest.main()
