fa526a1647cb503bb260a45e5c670583fecb4c9c
ReblochonMasque <trustgold@gmail.com>
2021-03-12 13:34:12 +0800

    relocate __str__ and __repr__ to Point2D and Vector2D

--- src/vector.py
+++ src/vector.py
@@ -62,11 +62,11 @@ class AbstractPointVector(ABC):
         """
         return not all(math.isclose(coord, 0, abs_tol=self.EPSILON) for coord in self._coords)
 
-    def __str__(self):
-        return f'{self.__class__.__name__}(x={self.x :.2f}, y={self.y :.2f})'
-
-    def __repr__(self):
-        return f'{self.__class__.__name__}(x={self.x}, y={self.y})'
+    # def __str__(self):
+    #     return f'{self.__class__.__name__}(x={self.x :.2f}, y={self.y :.2f})'
+    #
+    # def __repr__(self):
+    #     return f'{self.__class__.__name__}(x={self.x}, y={self.y})'
 
     def clone(self) -> 'AbstractPointVector':
         """clones self and returns it
@@ -77,6 +77,12 @@ class AbstractPointVector(ABC):
 
 
 class Point2D(AbstractPointVector):
+
+    def __str__(self):
+        return f'{self.__class__.__name__}(x={self.x :.2f}, y={self.y :.2f})'
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(x={self.x}, y={self.y})'
 
     def __add__(self, other: 'Vector2D') -> 'Point2D':
         """returns a new Vector2D sum of self and other
@@ -142,6 +148,12 @@ class Vector(AbstractPointVector):
 
 
 class Vector2D(Vector):
+
+    def __str__(self):
+        return f'{self.__class__.__name__}(x={self.x :.2f}, y={self.y :.2f})'
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}(x={self.x}, y={self.y})'
 
     def __add__(self, other: Union['Point2D', 'Vector2D']) -> Union['Point2D', 'Vector2D']:
         """returns a new Vector2D sum of self and other
