ca0a03d84a9a4ca9d234d040f26b0dab0a56c862
ReblochonMasque <trustgold@gmail.com>
2021-03-12 16:38:21 +0800

    add import Point to __all__

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,10 @@
 
-from numeric.src.vector import Point2D, Vector2D
+from numeric.src.vector import Point, Point2D, Vector, Vector2D
 
 
 __all__ = [
+    'Point',
+    'Vector',
     'Point2D',
     'Vector2D',
 ]
