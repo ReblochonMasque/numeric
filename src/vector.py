f37cb83f75491f4bc81e744922ac727c0e095dcf
ReblochonMasque <trustgold@gmail.com>
2021-03-13 15:34:26 +0800

    add __neg__

--- src/vector.py
+++ src/vector.py
@@ -137,6 +137,13 @@ class Vector(AbstractPointVector):
             raise TypeError("can only subtract a Vector from a Vector")
         return self.__class__(*(self_c - other_c for self_c, other_c in zip(self, other)))
 
+    def __neg__(self) -> 'Vector':
+        """creates a new Vector2D, negative of self, and returns it
+
+        :return: a new Vector2D, negative of self
+        """
+        return self.__class__(-coord for coord in self)
+
 
 class Vector2D(Vector):
 
@@ -150,13 +157,13 @@ class Vector2D(Vector):
 
     def __repr__(self):
         return f'{self.__class__.__name__}(x={self.x}, y={self.y})'
-
-    def __neg__(self) -> 'Vector2D':
-        """creates a new Vector2D, negative of self, and returns it
-
-        :return: a new Vector2D, negative of self
-        """
-        return self.__class__(-self.x, -self.y)
+    #
+    # def __neg__(self) -> 'Vector2D':
+    #     """creates a new Vector2D, negative of self, and returns it
+    #
+    #     :return: a new Vector2D, negative of self
+    #     """
+    #     return self.__class__(-self.x, -self.y)
 
     def __mul__(self, scalar: float) -> 'Vector2D':
         """returns a new Vector2D equal to self scaled by scalar
