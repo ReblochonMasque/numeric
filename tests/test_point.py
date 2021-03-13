6aa4ff02ccf5d772e48bf37a61a9b4fafc385854
ReblochonMasque <trustgold@gmail.com>
2021-03-13 09:58:41 +0800

    add test_iadd_Point2D_w_Point2D

--- tests/test_point.py
+++ tests/test_point.py
@@ -397,6 +397,15 @@ class TestPoint2DVector2DInteraction(unittest.TestCase):
         actual_id = id(actual)
         self.assertEqual(expected_id, actual_id)
 
+    def test_iadd_Point2D_w_Point2D(self):
+        """test Vector2D += Point2D"""
+        p0 = Point2D(4, 6)
+        p1 = Point2D(-6, -5)
+        with self.assertRaises(TypeError) as e:
+            p0 += p1
+        expected = "Can only mutate a Point via addition with a Vector"
+        self.assertEqual(expected, str(e.exception))
+
 
 if __name__ == '__main__':
     unittest.main()
