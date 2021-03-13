2fef52c69c5e07e1a9bec318bfd9feb4c14575af
ReblochonMasque <trustgold@gmail.com>
2021-03-13 14:38:01 +0800

    add test_iadd_mismatched_sizes_of_operands

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -178,6 +178,14 @@ class TestVector(unittest.TestCase):
         actual_id = id(actual)
         self.assertEqual(expected_id, actual_id)
 
+    def test_iadd_mismatched_sizes_of_operands(self):
+        expected = "mismatched sizes of operands"
+        v0 = Vector(7, 8, 0, 2, 1)
+        v1 = Vector(7, 6, 3, -2)
+        with self.assertRaises(ValueError) as e:
+            v0 += v1
+        self.assertEqual(expected, str(e.exception))
+
     def test_sub_0(self):
         expected = Vector(0, 2, -3, 4)
         v0 = Vector(7, 8, 0, 2)
