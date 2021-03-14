ec143b9b87f1db567ec0dc3946493c83e07d1988
ReblochonMasque <trustgold@gmail.com>
2021-03-14 10:55:59 +0800

    add test_truediv_1

--- tests/test_vector.py
+++ tests/test_vector.py
@@ -297,6 +297,12 @@ class TestVector(unittest.TestCase):
         actual = Vector(-1, 4, -7/3, -21) / -3
         self.assertEqual(expected, actual)
 
+    def test_truediv_1(self):
+        expected = "cannot divide a Vector by zero"
+        with self.assertRaises(ZeroDivisionError) as e:
+            _ = Vector(-1, 4, -7/3, -21) / 0
+        self.assertEqual(expected, str(e.exception))
+
 
 class TestVector2D(unittest.TestCase):
 
